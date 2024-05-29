5ea3ab9a9e43d9d7cbd924532d00cb13aed5e792
Mr Nitesh <159615163+mrxnitesh@users.noreply.github.com>
2024-05-29 08:12:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAF6hzh5NZNO4JWs77WYsNmQBVGsGuZfzc",
-    authDomain: "track-my-period.firebaseapp.com",
-    projectId: "track-my-period",
-    messagingSenderId: "963629924186",
-    appId: "1:963629924186:web:de3d1cfc765180b76e3d63"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
 document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('google-login-btn');
 
