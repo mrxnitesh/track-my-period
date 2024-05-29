9492751add0e3d63c404f7c7807ab017e142c435
Mr Nitesh <159615163+mrxnitesh@users.noreply.github.com>
2024-05-29 08:52:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyAF6hzh5NZNO4JWs77WYsNmQBVGsGuZfzc",
     authDomain: "track-my-period.firebaseapp.com",
     projectId: "track-my-period",
+    storageBucket: "track-my-period.appspot.com",
     messagingSenderId: "963629924186",
-    appId: "1:963629924186:web:de3d1cfc765180b76e3d63"
-};
+    appId: "1:963629924186:web:de3d1cfc765180b76e3d63",
+    measurementId: "G-S99Z27G8WF"
+  };
 
-// Initialize Firebase
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Auth service
 const auth = firebase.auth();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loginBtn = document.getElementById('google-login-btn');
+// Add event listener to the Google sign-in button
+const googleSignInButton = document.getElementById('google-login-btn');
+googleSignInButton.addEventListener('click', signInWithGoogle);
 
-    loginBtn.addEventListener('click', () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider)
-            .then((result) => {
-                // User signed in successfully
-                const user = result.user;
-                console.log(user);
-                // Redirect to the appropriate page after successful sign-in
-                window.location.href = "https://www.example.com/dashboard"; // Replace with your desired redirect URL
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+// Function to handle sign-in with Google
+function signInWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider)
+    .then((result) => {
+      // Sign-in successful
+      const user = result.user;
+      console.log('User signed in:', user);
+    })
+    .catch((error) => {
+      // Handle sign-in error
+      console.error('Sign-in error:', error);
     });
-});
+}
