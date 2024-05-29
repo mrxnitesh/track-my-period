c2cd4e237216b6a89c34993c3f565fd3b7bf3d36
Mr Nitesh <159615163+mrxnitesh@users.noreply.github.com>
2024-05-29 08:18:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAF6hzh5NZNO4JWs77WYsNmQBVGsGuZfzc",
+    authDomain: "track-my-period.firebaseapp.com",
+    projectId: "track-my-period",
+    messagingSenderId: "963629924186",
+    appId: "1:963629924186:web:de3d1cfc765180b76e3d63"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('google-login-btn');
 
@@ -8,9 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 // User signed in successfully
                 const user = result.user;
                 console.log(user);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    });
-});
+                // Redirect to the appropriate page after successful sign-in
+                window.location.href = "
+                    
